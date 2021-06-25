ff7f44914aa4457a5813cab9d9b3236851e07ff9
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2021-06-26 00:28:37 +0400

    Setup builder service. Docker Engine API connection.

---
+++ builder/src/main/java/io/imast/shoc/builder/services/EngineService.java
@@ -0,0 +1,33 @@
+package io.imast.shoc.builder.services;
+
+import io.imast.shoc.containerize.Containerize;
+import io.imast.shoc.containerize.EngineInstanceInfo;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The container engine service
+ * 
+ * @author davitp
+ */
+@Service
+public class EngineService {
+    
+    /**
+     * Get the dockerize instance
+     */
+    @Autowired
+    private Containerize dockerize;
+    
+    /**
+     * Gets all engine healths
+     * 
+     * @return Returns set of engine health instances
+     */
+    public List<EngineInstanceInfo> getAll(){
+        return Arrays.asList(this.dockerize.getInfo());
+    }
+    
+}
