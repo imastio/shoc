16938c445d359d6a75d173ab13bf8d7c3cf6fd2b
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2021-07-12 21:03:02 +0400

    added go version of shocctl

---
+++ shocctl/cmd/shocctl/shocctl.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Yet another command-line tool for Shoc"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Lookup",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					return err
+				}
+
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
