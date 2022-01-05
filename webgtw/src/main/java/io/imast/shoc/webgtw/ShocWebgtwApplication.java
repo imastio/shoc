925a6507e530052807fae9983a9e9109e3a29863
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2022-01-05 18:45:25 +0400

    Interim commit: created project for web gateway.

---
+++ webgtw/src/main/java/io/imast/shoc/webgtw/ShocWebgtwApplication.java
@@ -0,0 +1,22 @@
+package io.imast.shoc.webgtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Shoc application
+ * 
+ * @author Davit.Petrosyan
+ */
+@SpringBootApplication
+public class ShocWebgtwApplication {
+
+    /**
+     * The entry point for the application
+     * 
+     * @param args The application arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ShocWebgtwApplication.class, args);
+    }
+}
