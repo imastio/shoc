68188cb84c6365115089439f783a5ca9645720aa
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2022-03-05 20:58:00 +0400

    The spec changes

---
+++ Shoc.ModelCore/BuildInputSpec.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Shoc.ModelCore
+{
+    /// <summary>
+    /// The build input spec
+    /// </summary>
+    public class BuildInputSpec
+    {
+        /// <summary>
+        /// The files copy specification
+        /// </summary>
+        public List<FileCopySpec> Copy { get; set; }
+    }
+}
