cfe3f751e5e64ab651349891e08118017ca18597
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2024-09-26 01:12:16 +0400

    make dotenv quiet

--- cli/shoc-cli/Shoc.Cli/src/services/startup.ts
+++ cli/shoc-cli/Shoc.Cli/src/services/startup.ts
@@ -5,7 +5,7 @@ import https from 'https'
 
 export function startup() {
 
-    dotenv.config({convention: 'nextjs'});
+    dotenv.config({convention: 'nextjs', quiet: true});
 
     if (process.env.ALLOW_INSECURE === 'yes') {
         const httpsAgent = new https.Agent({
