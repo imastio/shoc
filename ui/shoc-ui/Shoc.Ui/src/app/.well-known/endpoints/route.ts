901ec1230fbe4736625e7ca62a0c1f7b14056276
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2025-04-30 18:55:21 +0400

    fix well-known endpoint

--- ui/shoc-ui/Shoc.Ui/src/app/.well-known/endpoints/route.ts
+++ ui/shoc-ui/Shoc.Ui/src/app/.well-known/endpoints/route.ts
@@ -4,9 +4,12 @@ import { shocApiConfig } from '@/clients/api-config';
 
 export async function GET() {
 
+  const idp = getIssuer();
+  const api = shocApiConfig().root
+
   const data = {
-    idp: getIssuer(),
-    api: shocApiConfig().root
+    idp: idp.endsWith('/') ? idp.substring(0, idp.length - 1) : idp,
+    api: api.endsWith('/') ? api.substring(0, idp.length - 1) : api
   };
 
   return NextResponse.json(data);
